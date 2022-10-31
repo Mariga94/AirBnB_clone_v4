a57f66acabfde9e32b412b060329c813f6f6c9ab
joseph <jsphmariga@gmail.com>
2022-10-31 23:14:41 +0000

    Add scriptz'

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,17 +1,18 @@
-$(document).ready(funtion () {
+const $ = window.$;
+$(document).ready(function () {
   const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
-  $.get(url, function(textStatus) {
-    if (textStatus.status == 'OK') {
+  $.get(url, function (textStatus) {
+    if (textStatus.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
-        $('DIV#api_status')removeClass('available');
-    })
-  let amenities = {};
-  if($('input[type="checkbox"]').prop('checked') == true){
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+  const amenities = {};
+  if ($('input[type="checkbox"]').prop('checked') === true) {
     amenities[$(this).attr('data-id')] = $(this).attr('data-name');
   } else {
-      delete amenities[$(this).attr('data-id')];
+    delete amenities[$(this).attr('data-id')];
   }
   $('.amenities H4').text(Object.values(amenities).join(','));
-  
 });
